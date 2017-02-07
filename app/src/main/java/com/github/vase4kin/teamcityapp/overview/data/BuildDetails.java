543b3d78bdb5abeff8df6603a42c526f6849e8ab
Andrey Tolpeev <andrey.tolpeev@gmail.com>
2017-02-07 22:33:12 +0700

    Add missed files

---
+++ app/src/main/java/com/github/vase4kin/teamcityapp/overview/data/BuildDetails.java
@@ -0,0 +1,458 @@
+/*
+ * Copyright 2016 Andrey Tolpeev
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *        http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.github.vase4kin.teamcityapp.overview.data;
+
+import android.support.annotation.Nullable;
+
+import com.github.vase4kin.teamcityapp.base.api.Jsonable;
+import com.github.vase4kin.teamcityapp.buildlist.api.Build;
+import com.github.vase4kin.teamcityapp.properties.api.Properties;
+
+import java.io.Serializable;
+
+/**
+ * Interface to handle build details
+ */
+public interface BuildDetails extends Jsonable, Serializable {
+    /**
+     * @return {String} as server build url
+     */
+    String getHref();
+
+    /**
+     * @return {String} as server build web url
+     */
+    String getWebUrl();
+
+    /**
+     * @return build changes url as {String}
+     */
+    String getChangesHref();
+
+    /**
+     * @return build status text as {String}
+     */
+    String getStatusText();
+
+    /**
+     * @return as status icon as {String}
+     */
+    String getStatusIcon();
+
+    /**
+     * @return {true} if build is running
+     */
+    boolean isRunning();
+
+    /**
+     * @return {true} if build is queued
+     */
+    boolean isQueued();
+
+    /**
+     * @return {true} if build is success
+     */
+    boolean isSuccess();
+
+    /**
+     * @return {true} if build is failed
+     */
+    boolean isFailed();
+
+    /**
+     * @return {true} if build has cancellation info
+     */
+    boolean hasCancellationInfo();
+
+    /**
+     * @return {true} if was cancelled by user
+     */
+    boolean hasUserInfoWhoCancelledBuild();
+
+    /**
+     * @return user name as {String}
+     */
+    String getUserNameWhoCancelledBuild();
+
+    /**
+     * @return cancellation time as {String}
+     */
+    String getCancellationTime();
+
+    /**
+     * @return start date as {String}
+     */
+    String getStartDate();
+
+    /**
+     * @return queued date as {String}
+     */
+    String getQueuedDate();
+
+    /**
+     * @return finish date as {String}
+     */
+    String getFinishTime();
+
+    /**
+     * @return estimated start time as {String}. Can be null
+     */
+    @Nullable
+    String getEstimatedStartTime();
+
+    /**
+     * @return branch name as {String}. Can be null
+     */
+    @Nullable
+    String getBranchName();
+
+    /**
+     * @return {true} if build has agent
+     */
+    boolean hasAgentInfo();
+
+    /**
+     * @return agent name as {String}
+     */
+    String getAgentName();
+
+    /**
+     * @return {true} if build was triggered by vcs
+     */
+    boolean isTriggeredByVcs();
+
+    /**
+     * @return {true} if build was triggered by user
+     */
+    boolean isTriggeredByUser();
+
+    /**
+     * @return {true} if build was restarted
+     */
+    boolean isRestarted();
+
+    /**
+     * @return {true} if build was triggered by build type
+     */
+    boolean isTriggeredByBuildType();
+
+    /**
+     * @return {true} if build was triggered by unknown
+     */
+    boolean isTriggeredByUnknown();
+
+    /**
+     * @return triggered details as {String}
+     */
+    String getTriggeredDetails();
+
+    /**
+     * @return User name of the user who triggered build
+     */
+    String getUserNameOfUserWhoTriggeredBuild();
+
+    /**
+     * @return Name of configuration who triggered build
+     */
+    String getNameOfTriggeredBuildType();
+
+    /**
+     * @return id of configuration
+     */
+    String getBuildTypeId();
+
+    /**
+     * @return id of the build
+     */
+    String getId();
+
+    Properties getProperties();
+
+    /**
+     * @return {true} if the build has tests
+     */
+    boolean hasTests();
+
+    /**
+     * @return build tests href as {String}
+     */
+    String getTestsHref();
+
+    /**
+     * @return passed tests count as {int}
+     */
+    int getPassedTestCount();
+
+    /**
+     * @return failed tests count as {int}
+     */
+    int getFailedTestCount();
+
+    /**
+     * @return ignored tests count as {int}
+     */
+    int getIgnoredTestCount();
+
+    /**
+     * @param userName - User name
+     * @return {true} if build was triggered by user with name
+     */
+    boolean isTriggeredByUser(String userName);
+
+    /**
+     * @return build artifacts href as {String}
+     */
+    String getArtifactsHref();
+
+    /**
+     * @return build number as {String}
+     */
+    String getNumber();
+
+    /**
+     * @return {true} if the build is personal
+     */
+    boolean isPersonal();
+
+    /**
+     * @return {true} if the build is pinned
+     */
+    boolean isPinned();
+
+    /**
+     * @return build wrapped by build details
+     */
+    Build toBuild();
+
+    /**
+     * Stub
+     */
+    BuildDetails STUB = new BuildDetails() {
+        @Override
+        public String getHref() {
+            return null;
+        }
+
+        @Override
+        public String getWebUrl() {
+            return null;
+        }
+
+        @Override
+        public String getChangesHref() {
+            return null;
+        }
+
+        @Override
+        public String getStatusText() {
+            return null;
+        }
+
+        @Override
+        public String getStatusIcon() {
+            return null;
+        }
+
+        @Override
+        public boolean isRunning() {
+            return false;
+        }
+
+        @Override
+        public boolean isQueued() {
+            return false;
+        }
+
+        @Override
+        public boolean isSuccess() {
+            return false;
+        }
+
+        @Override
+        public boolean isFailed() {
+            return false;
+        }
+
+        @Override
+        public boolean hasCancellationInfo() {
+            return false;
+        }
+
+        @Override
+        public boolean hasUserInfoWhoCancelledBuild() {
+            return false;
+        }
+
+        @Override
+        public String getUserNameWhoCancelledBuild() {
+            return null;
+        }
+
+        @Override
+        public String getCancellationTime() {
+            return null;
+        }
+
+        @Override
+        public String getStartDate() {
+            return null;
+        }
+
+        @Override
+        public String getQueuedDate() {
+            return null;
+        }
+
+        @Override
+        public String getFinishTime() {
+            return null;
+        }
+
+        @Nullable
+        @Override
+        public String getEstimatedStartTime() {
+            return null;
+        }
+
+        @Nullable
+        @Override
+        public String getBranchName() {
+            return null;
+        }
+
+        @Override
+        public boolean hasAgentInfo() {
+            return false;
+        }
+
+        @Override
+        public String getAgentName() {
+            return null;
+        }
+
+        @Override
+        public boolean isTriggeredByVcs() {
+            return false;
+        }
+
+        @Override
+        public boolean isTriggeredByUser() {
+            return false;
+        }
+
+        @Override
+        public boolean isRestarted() {
+            return false;
+        }
+
+        @Override
+        public boolean isTriggeredByBuildType() {
+            return false;
+        }
+
+        @Override
+        public boolean isTriggeredByUnknown() {
+            return false;
+        }
+
+        @Override
+        public String getTriggeredDetails() {
+            return null;
+        }
+
+        @Override
+        public String getUserNameOfUserWhoTriggeredBuild() {
+            return null;
+        }
+
+        @Override
+        public String getNameOfTriggeredBuildType() {
+            return null;
+        }
+
+        @Override
+        public String getBuildTypeId() {
+            return null;
+        }
+
+        @Override
+        public String getId() {
+            return null;
+        }
+
+        @Override
+        public Properties getProperties() {
+            return null;
+        }
+
+        @Override
+        public boolean hasTests() {
+            return false;
+        }
+
+        @Override
+        public String getTestsHref() {
+            return null;
+        }
+
+        @Override
+        public int getPassedTestCount() {
+            return 0;
+        }
+
+        @Override
+        public int getFailedTestCount() {
+            return 0;
+        }
+
+        @Override
+        public int getIgnoredTestCount() {
+            return 0;
+        }
+
+        @Override
+        public boolean isTriggeredByUser(String userName) {
+            return false;
+        }
+
+        @Override
+        public String getArtifactsHref() {
+            return null;
+        }
+
+        @Override
+        public String getNumber() {
+            return null;
+        }
+
+        @Override
+        public boolean isPersonal() {
+            return false;
+        }
+
+        @Override
+        public boolean isPinned() {
+            return false;
+        }
+
+        @Override
+        public Build toBuild() {
+            return null;
+        }
+    };
+
+}
---
+++ app/src/test/java/com/github/vase4kin/teamcityapp/overview/data/BuildDetailsImplTest.java
@@ -0,0 +1,399 @@
+/*
+ * Copyright 2016 Andrey Tolpeev
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *        http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.github.vase4kin.teamcityapp.overview.data;
+
+import com.github.vase4kin.teamcityapp.agents.api.Agent;
+import com.github.vase4kin.teamcityapp.artifact.api.Artifacts;
+import com.github.vase4kin.teamcityapp.buildlist.api.Build;
+import com.github.vase4kin.teamcityapp.buildlist.api.CanceledInfo;
+import com.github.vase4kin.teamcityapp.buildlist.api.Triggered;
+import com.github.vase4kin.teamcityapp.buildlist.api.User;
+import com.github.vase4kin.teamcityapp.changes.api.Changes;
+import com.github.vase4kin.teamcityapp.navigation.api.BuildType;
+import com.github.vase4kin.teamcityapp.properties.api.Properties;
+import com.github.vase4kin.teamcityapp.tests.api.TestOccurrences;
+import com.github.vase4kin.teamcityapp.utils.IconUtils;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+
+import static com.github.vase4kin.teamcityapp.overview.data.BuildDetailsImpl.NO_NUMBER;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.equalTo;
+import static org.hamcrest.Matchers.is;
+import static org.mockito.Mockito.when;
+
+public class BuildDetailsImplTest {
+
+    /**
+     * Test data
+     */
+    private static final String TIME_STAMP = "20160621T233008+0700";
+    private static final String TIME_FORMATTED = "21 Jun 16 23:30";
+
+    @Mock
+    private Artifacts mArtifacts;
+
+    @Mock
+    private TestOccurrences mTestOccurrences;
+
+    @Mock
+    private Changes mChanges;
+
+    @Mock
+    private Properties mProperties;
+
+    @Mock
+    private Triggered mTriggered;
+
+    @Mock
+    private BuildType mBuildType;
+
+    @Mock
+    private Agent mAgent;
+
+    @Mock
+    private User mUser;
+
+    @Mock
+    private CanceledInfo mCanceledInfo;
+
+    @Mock
+    private Build mBuild;
+
+    private BuildDetails mBuildDetails;
+
+    @Before
+    public void setUp() throws Exception {
+        MockitoAnnotations.initMocks(this);
+        mBuildDetails = new BuildDetailsImpl(mBuild);
+    }
+
+    @Test
+    public void testGetHref() throws Exception {
+        when(mBuild.getHref()).thenReturn("href");
+        assertThat(mBuildDetails.getHref(), is(equalTo("href")));
+    }
+
+    @Test
+    public void testGetWebUrl() throws Exception {
+        when(mBuild.getWebUrl()).thenReturn("webUrl");
+        assertThat(mBuildDetails.getWebUrl(), is(equalTo("webUrl")));
+    }
+
+    @Test
+    public void testGetChangesHref() throws Exception {
+        when(mBuild.getChanges()).thenReturn(mChanges);
+        when(mChanges.getHref()).thenReturn("href");
+        assertThat(mBuildDetails.getChangesHref(), is(equalTo("href")));
+    }
+
+    @Test
+    public void testGetStatusTextIfBuildIsNotQueued() throws Exception {
+        when(mBuild.getStatusText()).thenReturn("status");
+        assertThat(mBuildDetails.getStatusText(), is(equalTo("status")));
+    }
+
+    @Test
+    public void testGetStatusIcon() throws Exception {
+        when(mBuild.getState()).thenReturn("");
+        when(mBuild.getStatus()).thenReturn("");
+        assertThat(mBuildDetails.getStatusIcon(), is(equalTo(IconUtils.SUCCESS)));
+    }
+
+    @Test
+    public void testIsRunning() throws Exception {
+        when(mBuild.isRunning()).thenReturn(true);
+        assertThat(mBuildDetails.isRunning(), is(equalTo(true)));
+    }
+
+    @Test
+    public void testIsQueued() throws Exception {
+        when(mBuild.isQueued()).thenReturn(true);
+        assertThat(mBuildDetails.isQueued(), is(equalTo(true)));
+    }
+
+    @Test
+    public void testIsSuccess() throws Exception {
+        when(mBuild.isSuccess()).thenReturn(true);
+        assertThat(mBuildDetails.isSuccess(), is(equalTo(true)));
+    }
+
+    @Test
+    public void testIsFailed() throws Exception {
+        when(mBuild.isFailed()).thenReturn(true);
+        assertThat(mBuildDetails.isFailed(), is(equalTo(true)));
+    }
+
+    @Test
+    public void testHasCancellationInfo() throws Exception {
+        when(mBuild.getCanceledInfo()).thenReturn(mCanceledInfo);
+        assertThat(mBuildDetails.hasCancellationInfo(), is(equalTo(true)));
+    }
+
+    @Test
+    public void testHasUserInfoWhoCancelledBuild() throws Exception {
+        when(mBuild.getCanceledInfo()).thenReturn(mCanceledInfo);
+        when(mCanceledInfo.getUser()).thenReturn(mUser);
+        assertThat(mBuildDetails.hasUserInfoWhoCancelledBuild(), is(equalTo(true)));
+    }
+
+    @Test
+    public void testGetUserNameWhoCancelledBuild() throws Exception {
+        when(mBuild.getCanceledInfo()).thenReturn(mCanceledInfo);
+        when(mCanceledInfo.getUser()).thenReturn(mUser);
+        when(mUser.getName()).thenReturn("user");
+        assertThat(mBuildDetails.getUserNameWhoCancelledBuild(), is(equalTo("user")));
+    }
+
+    @Test
+    public void testGetCancellationTime() throws Exception {
+        when(mBuild.getCanceledInfo()).thenReturn(mCanceledInfo);
+        when(mCanceledInfo.getTimestamp()).thenReturn(TIME_STAMP);
+        assertThat(mBuildDetails.getCancellationTime(), is(equalTo(TIME_FORMATTED)));
+    }
+
+    @Test
+    public void testGetStartDate() throws Exception {
+        when(mBuild.getStartDate()).thenReturn(TIME_STAMP);
+        assertThat(mBuildDetails.getStartDate(), is(equalTo(TIME_FORMATTED)));
+    }
+
+    @Test
+    public void testGetQueuedDate() throws Exception {
+        when(mBuild.getQueuedDate()).thenReturn(TIME_STAMP);
+        assertThat(mBuildDetails.getQueuedDate(), is(equalTo(TIME_FORMATTED)));
+    }
+
+    @Test
+    public void testGetFinishTime() throws Exception {
+        when(mBuild.getStartDate()).thenReturn(TIME_STAMP);
+        when(mBuild.getFinishDate()).thenReturn(TIME_STAMP);
+        assertThat(mBuildDetails.getFinishTime(), is(equalTo("21 Jun 16 23:30 - 23:30 (0s)")));
+    }
+
+    @Test
+    public void testGetEstimatedStartTime() throws Exception {
+        when(mBuild.getStartEstimate()).thenReturn(TIME_STAMP);
+        assertThat(mBuildDetails.getEstimatedStartTime(), is(equalTo(TIME_FORMATTED)));
+    }
+
+    @Test
+    public void testGetBranchName() throws Exception {
+        when(mBuild.getBranchName()).thenReturn("branch");
+        assertThat(mBuildDetails.getBranchName(), is(equalTo("branch")));
+    }
+
+    @Test
+    public void testHasAgentInfo() throws Exception {
+        when(mBuild.getAgent()).thenReturn(mAgent);
+        assertThat(mBuildDetails.hasAgentInfo(), is(equalTo(true)));
+    }
+
+    @Test
+    public void testGetAgentName() throws Exception {
+        when(mBuild.getAgent()).thenReturn(mAgent);
+        when(mAgent.getName()).thenReturn("name");
+        assertThat(mBuildDetails.getAgentName(), is(equalTo("name")));
+    }
+
+    @Test
+    public void testIsTriggeredByVcs() throws Exception {
+        when(mBuild.getTriggered()).thenReturn(mTriggered);
+        when(mTriggered.isVcs()).thenReturn(true);
+        assertThat(mBuildDetails.isTriggeredByVcs(), is(equalTo(true)));
+    }
+
+    @Test
+    public void testIsTriggeredByUser() throws Exception {
+        when(mBuild.getTriggered()).thenReturn(mTriggered);
+        when(mTriggered.isUser()).thenReturn(true);
+        assertThat(mBuildDetails.isTriggeredByUser(), is(equalTo(true)));
+    }
+
+    @Test
+    public void testIsRestarted() throws Exception {
+        when(mBuild.getTriggered()).thenReturn(mTriggered);
+        when(mTriggered.isRestarted()).thenReturn(true);
+        assertThat(mBuildDetails.isRestarted(), is(equalTo(true)));
+    }
+
+    @Test
+    public void testIsTriggeredByBuildType() throws Exception {
+        when(mBuild.getTriggered()).thenReturn(mTriggered);
+        when(mTriggered.isBuildType()).thenReturn(true);
+        assertThat(mBuildDetails.isTriggeredByBuildType(), is(equalTo(true)));
+    }
+
+    @Test
+    public void testIsTriggeredByUnknown() throws Exception {
+        when(mBuild.getTriggered()).thenReturn(mTriggered);
+        when(mTriggered.isUnknown()).thenReturn(true);
+        assertThat(mBuildDetails.isTriggeredByUnknown(), is(equalTo(true)));
+    }
+
+    @Test
+    public void tetsGetTriggeredDetails() throws Exception {
+        when(mBuild.getTriggered()).thenReturn(mTriggered);
+        when(mTriggered.getDetails()).thenReturn("details");
+        assertThat(mBuildDetails.getTriggeredDetails(), is(equalTo("details")));
+    }
+
+    @Test
+    public void testGetUserNameOfUserWhoTriggeredBuild() throws Exception {
+        when(mBuild.getTriggered()).thenReturn(mTriggered);
+        when(mTriggered.getUser()).thenReturn(mUser);
+        when(mUser.getName()).thenReturn("name");
+        assertThat(mBuildDetails.getUserNameOfUserWhoTriggeredBuild(), is(equalTo("name")));
+    }
+
+    @Test
+    public void testGetNameOfTriggeredBuildTypeIfBuildTypeIsDeleted() throws Exception {
+        when(mBuild.getTriggered()).thenReturn(mTriggered);
+        when(mTriggered.getBuildType()).thenReturn(null);
+        assertThat(mBuildDetails.getNameOfTriggeredBuildType(), is(equalTo(BuildDetailsImpl.DELETED_CONFIGURATION)));
+    }
+
+    @Test
+    public void testGetNameOfTriggeredBuildType() throws Exception {
+        when(mBuild.getTriggered()).thenReturn(mTriggered);
+        when(mTriggered.getBuildType()).thenReturn(mBuildType);
+        when(mBuildType.getProjectName()).thenReturn("pn");
+        when(mBuildType.getName()).thenReturn("cn");
+        assertThat(mBuildDetails.getNameOfTriggeredBuildType(), is(equalTo("pn cn")));
+    }
+
+    @Test
+    public void testGetBuildTypeId() throws Exception {
+        when(mBuild.getBuildTypeId()).thenReturn("id");
+        assertThat(mBuildDetails.getBuildTypeId(), is(equalTo("id")));
+    }
+
+    @Test
+    public void testGetBuildId() throws Exception {
+        when(mBuild.getId()).thenReturn("id");
+        assertThat(mBuildDetails.getId(), is(equalTo("id")));
+    }
+
+    @Test
+    public void testGetProperties() throws Exception {
+        when(mBuild.getProperties()).thenReturn(mProperties);
+        assertThat(mBuildDetails.getProperties(), is(equalTo(mProperties)));
+    }
+
+    @Test
+    public void testIsTriggeredByUserIfTriggeredIsNull() throws Exception {
+        when(mBuild.getTriggered()).thenReturn(null);
+        assertThat(mBuildDetails.isTriggeredByUser("user"), is(equalTo(false)));
+    }
+
+    @Test
+    public void testIsTriggeredByUserIfTriggeredByNotUser() throws Exception {
+        when(mBuild.getTriggered()).thenReturn(mTriggered);
+        when(mTriggered.isUser()).thenReturn(false);
+        assertThat(mBuildDetails.isTriggeredByUser("user"), is(equalTo(false)));
+    }
+
+    @Test
+    public void testIsTriggeredByUserIfUserIsNull() throws Exception {
+        when(mBuild.getTriggered()).thenReturn(mTriggered);
+        when(mTriggered.isUser()).thenReturn(true);
+        when(mTriggered.getUser()).thenReturn(null);
+        assertThat(mBuildDetails.isTriggeredByUser("user"), is(equalTo(false)));
+    }
+
+    @Test
+    public void testIsTriggeredByUserIfUserIsNotNull() throws Exception {
+        when(mBuild.getTriggered()).thenReturn(mTriggered);
+        when(mTriggered.isUser()).thenReturn(true);
+        when(mTriggered.getUser()).thenReturn(new User("user", null));
+        assertThat(mBuildDetails.isTriggeredByUser("user"), is(equalTo(true)));
+    }
+
+    @Test
+    public void testHasTests() throws Exception {
+        when(mBuild.getTestOccurrences()).thenReturn(mTestOccurrences);
+        assertThat(mBuildDetails.hasTests(), is(equalTo(true)));
+    }
+
+    @Test
+    public void testGetTestsHref() throws Exception {
+        when(mBuild.getTestOccurrences()).thenReturn(mTestOccurrences);
+        when(mTestOccurrences.getHref()).thenReturn("href");
+        assertThat(mBuildDetails.getTestsHref(), is(equalTo("href")));
+    }
+
+    @Test
+    public void testGetPassedTestsCount() throws Exception {
+        when(mBuild.getTestOccurrences()).thenReturn(mTestOccurrences);
+        when(mTestOccurrences.getPassed()).thenReturn(3);
+        assertThat(mBuildDetails.getPassedTestCount(), is(equalTo(3)));
+    }
+
+    @Test
+    public void testGetFailedTestsCount() throws Exception {
+        when(mBuild.getTestOccurrences()).thenReturn(mTestOccurrences);
+        when(mTestOccurrences.getFailed()).thenReturn(4);
+        assertThat(mBuildDetails.getFailedTestCount(), is(equalTo(4)));
+    }
+
+    @Test
+    public void testGetIgnoredTestsCount() throws Exception {
+        when(mBuild.getTestOccurrences()).thenReturn(mTestOccurrences);
+        when(mTestOccurrences.getIgnored()).thenReturn(5);
+        assertThat(mBuildDetails.getIgnoredTestCount(), is(equalTo(5)));
+    }
+
+    @Test
+    public void testGetArtifactsHref() throws Exception {
+        when(mBuild.getArtifacts()).thenReturn(mArtifacts);
+        when(mArtifacts.getHref()).thenReturn("href");
+        assertThat(mBuildDetails.getArtifactsHref(), is(equalTo("href")));
+    }
+
+    @Test
+    public void testGetNumber() throws Exception {
+        when(mBuild.getNumber()).thenReturn("Number");
+        assertThat(mBuildDetails.getNumber(), is(equalTo("Number")));
+    }
+
+    @Test
+    public void testGetNumberIfNumberIsNull() throws Exception {
+        when(mBuild.getNumber()).thenReturn(null);
+        assertThat(mBuildDetails.getNumber(), is(equalTo(NO_NUMBER)));
+    }
+
+    @Test
+    public void testIsPersonal() throws Exception {
+        when(mBuild.isPersonal()).thenReturn(true);
+        assertThat(mBuildDetails.isPersonal(), is(equalTo(true)));
+    }
+
+    @Test
+    public void testIsPinned() throws Exception {
+        when(mBuild.isPinned()).thenReturn(true);
+        assertThat(mBuildDetails.isPinned(), is(equalTo(true)));
+    }
+
+    @Test
+    public void testToBuild() throws Exception {
+        assertThat(mBuildDetails.toBuild(), is(equalTo(mBuild)));
+    }
+
+}
