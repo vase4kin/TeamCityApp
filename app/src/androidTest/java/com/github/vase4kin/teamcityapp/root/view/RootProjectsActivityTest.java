8118c16e9f7c32abfe961fd9c51da55b0b4b0f06
Andrey Tolpeev <andrey.tolpeev@gmail.com>
2018-06-10 01:00:54 +0800

    Update UI tests

--- app/src/androidTest/java/com/github/vase4kin/teamcityapp/root/view/RootProjectsActivityTest.java
+++ app/src/androidTest/java/com/github/vase4kin/teamcityapp/root/view/RootProjectsActivityTest.java
@@ -17,6 +17,8 @@
 package com.github.vase4kin.teamcityapp.root.view;
 
 import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.support.test.InstrumentationRegistry;
 import android.support.test.runner.AndroidJUnit4;
 
@@ -29,7 +31,7 @@ import com.github.vase4kin.teamcityapp.dagger.modules.AppModule;
 import com.github.vase4kin.teamcityapp.dagger.modules.FakeTeamCityServiceImpl;
 import com.github.vase4kin.teamcityapp.dagger.modules.Mocks;
 import com.github.vase4kin.teamcityapp.dagger.modules.RestApiModule;
-import com.github.vase4kin.teamcityapp.helper.CustomActivityTestRule;
+import com.github.vase4kin.teamcityapp.helper.CustomIntentsTestRule;
 import com.github.vase4kin.teamcityapp.helper.TestUtils;
 import com.github.vase4kin.teamcityapp.navigation.api.BuildType;
 import com.github.vase4kin.teamcityapp.navigation.api.BuildTypes;
@@ -54,11 +56,18 @@ import rx.Observable;
 import static android.support.test.espresso.Espresso.onView;
 import static android.support.test.espresso.action.ViewActions.click;
 import static android.support.test.espresso.assertion.ViewAssertions.matches;
+import static android.support.test.espresso.intent.Intents.intended;
+import static android.support.test.espresso.intent.matcher.IntentMatchers.hasAction;
+import static android.support.test.espresso.intent.matcher.IntentMatchers.hasData;
+import static android.support.test.espresso.matcher.ViewMatchers.assertThat;
 import static android.support.test.espresso.matcher.ViewMatchers.withId;
 import static android.support.test.espresso.matcher.ViewMatchers.withText;
 import static com.github.vase4kin.teamcityapp.helper.RecyclerViewMatcher.withRecyclerView;
 import static com.github.vase4kin.teamcityapp.helper.TestUtils.hasItemsCount;
 import static com.github.vase4kin.teamcityapp.helper.TestUtils.matchToolbarTitle;
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.core.AllOf.allOf;
 import static org.mockito.Matchers.anyString;
 import static org.mockito.Mockito.when;
 
@@ -80,7 +89,7 @@ public class RootProjectsActivityTest {
             });
 
     @Rule
-    public CustomActivityTestRule<RootProjectsActivity> mActivityRule = new CustomActivityTestRule<>(RootProjectsActivity.class);
+    public CustomIntentsTestRule<RootProjectsActivity> mActivityRule = new CustomIntentsTestRule<>(RootProjectsActivity.class);
 
     @Spy
     private TeamCityService mTeamCityService = new FakeTeamCityServiceImpl();
@@ -98,7 +107,7 @@ public class RootProjectsActivityTest {
         TeamCityApplication app = (TeamCityApplication) InstrumentationRegistry.getInstrumentation().getTargetContext().getApplicationContext();
         app.getAppInjector().sharedUserStorage().clearAll();
         app.getRestApiInjector().sharedUserStorage().saveGuestUserAccountAndSetItAsActive(Mocks.URL, false);
-        app.getSharedPreferences("rateTheAppPref", Context.MODE_PRIVATE).edit().clear().commit();
+        getSharedPreferences().edit().clear().commit();
     }
 
     @Test
@@ -196,6 +205,9 @@ public class RootProjectsActivityTest {
         // Cancel rate the app
         onView(withId(R.id.button_cancel)).perform(click());
 
+        // Check the flag is set
+        assertThat(getSharedPreferences().getBoolean("rated", false), is(equalTo(true)));
+
         // Checking data
         onView(withId(R.id.navigation_recycler_view)).check(hasItemsCount(2));
         onView(withRecyclerView(R.id.navigation_recycler_view).atPositionOnView(0, R.id.itemTitle))
@@ -204,6 +216,7 @@ public class RootProjectsActivityTest {
                 .check(matches(withText("Description")));
         onView(withRecyclerView(R.id.navigation_recycler_view).atPositionOnView(1, R.id.itemTitle))
                 .check(matches(withText("build type")));
+
 
         // Click on projects
         onView(withText("Project"))
@@ -217,5 +230,39 @@ public class RootProjectsActivityTest {
                 .check(matches(withText("Contains a lof of projects")));
         onView(withRecyclerView(R.id.navigation_recycler_view).atPositionOnView(1, R.id.itemTitle))
                 .check(matches(withText("Build and run tests")));
+    }
+
+    @Test
+    public void testUserCanRateTheApp() {
+        // Prepare data
+        Project project = new Project();
+        project.setName("New project");
+        project.setDescription("Contains a lof of projects");
+        BuildType buildType = new BuildType();
+        buildType.setId("build_type_id");
+        buildType.setName("Build and run tests");
+        NavigationNode navigationNode = new NavigationNode(
+                new Projects(Collections.singletonList(project)),
+                new BuildTypes(Collections.singletonList(buildType)));
+        when(mTeamCityService.listBuildTypes(anyString())).thenCallRealMethod().thenReturn(Observable.just(navigationNode));
+        when(remoteService.isNotChurn()).thenReturn(true);
+
+        mActivityRule.launchActivity(null);
+
+        // Rate the app
+        onView(withId(R.id.button_rate)).perform(click());
+
+        // Check the flag is set
+        assertThat(getSharedPreferences().getBoolean("rated", false), is(equalTo(true)));
+
+        // Check filter builds activity is opened
+        intended(allOf(
+                hasAction(Intent.ACTION_VIEW),
+                hasData("market://details?id=com.github.vase4kin.teamcityapp.mock.debug")));
+    }
+
+    private SharedPreferences getSharedPreferences() {
+        TeamCityApplication app = (TeamCityApplication) InstrumentationRegistry.getInstrumentation().getTargetContext().getApplicationContext();
+        return app.getSharedPreferences("rateTheAppPref", Context.MODE_PRIVATE);
     }
 }
