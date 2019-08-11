84f518b9e7124d7f57e64d991844fae623244f38
Andrey Tolpeev <andrey.tolpeev@gmail.com>
2019-08-11 23:23:17 +0800

    Convert java classes to kotlin

--- app/src/main/java/com/github/vase4kin/teamcityapp/account/create/data/OnLoadingListener.kt
+++ app/src/main/java/com/github/vase4kin/teamcityapp/account/create/data/OnLoadingListener.kt
@@ -1,11 +1,11 @@
 /*
- * Copyright 2016 Andrey Tolpeev
+ * Copyright 2019 Andrey Tolpeev
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
- *        http://www.apache.org/licenses/LICENSE-2.0
+ *    http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
@@ -14,25 +14,24 @@
  * limitations under the License.
  */
 
-package com.github.vase4kin.teamcityapp.account.create.data;
+package com.github.vase4kin.teamcityapp.account.create.data
 
 /**
- * Custom listener to handle error with status code
+ * listener to receive loading callbacks
  */
-public interface CustomOnLoadingListener<T> {
+interface OnLoadingListener<T> {
 
     /**
      * On success callback
      *
      * @param data - Data is passed to success
      */
-    void onSuccess(T data);
+    fun onSuccess(data: T)
 
     /**
      * On fail callback
      *
-     * @param statusCode   - Error status code
      * @param errorMessage - Error message
      */
-    void onFail(int statusCode, String errorMessage);
+    fun onFail(errorMessage: String)
 }
---
+++ app/src/main/java/com/github/vase4kin/teamcityapp/account/create/helper/UrlFormatterImpl.kt
@@ -0,0 +1,56 @@
+/*
+ * Copyright 2019 Andrey Tolpeev
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.github.vase4kin.teamcityapp.account.create.helper
+
+import android.net.Uri
+import android.text.TextUtils
+
+import javax.inject.Inject
+
+/**
+ * Impl of [UrlFormatter]
+ */
+class UrlFormatterImpl @Inject constructor() : UrlFormatter {
+
+    /**
+     * {@inheritDoc}
+     */
+    override fun formatServerUrl(serverUrl: String): String {
+        val serverUri = Uri.parse(serverUrl)
+        val hasNoPathSegment = TextUtils.isEmpty(serverUri.lastPathSegment)
+        return if (hasNoPathSegment) {
+            // Url without path - http://teamcity.com
+            serverUri.toString()
+        } else {
+            // Url with path - https://teamcity.com/server
+            // To build such url with retrofit we need to add / at the end
+            serverUri.buildUpon().appendPath("").toString()
+        }
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    override fun formatBasicUrl(url: String): String {
+        // Check that url has leading slash
+        return if (url.startsWith("/")) {
+            url.replaceFirst("/".toRegex(), "")
+        } else {
+            url
+        }
+    }
+}
