dbed380b3e1c6ae35ddd0fcedc90a4c493b9d848
Andrey Tolpeev <andrey.tolpeev@gmail.com>
2019-08-12 12:40:31 +0800

    Fix compilation issues

--- app/src/main/java/com/github/vase4kin/teamcityapp/runbuild/interactor/RunBuildInteractorImpl.kt
+++ app/src/main/java/com/github/vase4kin/teamcityapp/runbuild/interactor/RunBuildInteractorImpl.kt
@@ -1,11 +1,11 @@
 /*
- * Copyright 2016 Andrey Tolpeev
+ * Copyright 2019 Andrey Tolpeev
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
- *        http://www.apache.org/licenses/LICENSE-2.0
+ *    http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
@@ -93,10 +93,10 @@ class RunBuildInteractorImpl(
                                 if (e.code() == CODE_FORBIDDEN) {
                                     loadingListener.onForbiddenError()
                                 } else {
-                                    loadingListener.onFail(e.message)
+                                    loadingListener.onFail(e.message ?: "")
                                 }
                             } else {
-                                loadingListener.onFail(e.message)
+                                loadingListener.onFail(e.message ?: "")
                             }
                         }
                 )
@@ -129,7 +129,7 @@ class RunBuildInteractorImpl(
                 .observeOn(AndroidSchedulers.mainThread())
                 .subscribeBy(
                         onSuccess = { loadingListener.onSuccess(it.objects) },
-                        onError = { loadingListener.onFail(it.message) }
+                        onError = { loadingListener.onFail(it.message ?: "") }
                 )
                 .addTo(subscriptions)
     }
