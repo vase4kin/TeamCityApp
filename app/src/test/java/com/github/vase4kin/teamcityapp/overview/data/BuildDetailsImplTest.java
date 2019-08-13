c1bacda95beea73d23e7f9bcd2d24b449244dc4d
Andrey Tolpeev <andrey.tolpeev@gmail.com>
2019-08-13 22:10:15 +0800

    Fix unit tests

--- app/src/test/java/com/github/vase4kin/teamcityapp/overview/data/BuildDetailsImplTest.java
+++ app/src/test/java/com/github/vase4kin/teamcityapp/overview/data/BuildDetailsImplTest.java
@@ -1,11 +1,11 @@
 /*
- * Copyright 2016 Andrey Tolpeev
+ * Copyright 2019 Andrey Tolpeev
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
- *        http://www.apache.org/licenses/LICENSE-2.0
+ *    http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
@@ -70,8 +70,6 @@ public class BuildDetailsImplTest {
     @Mock
     private BuildType mBuildType;
 
-    @Mock
-    private Agent mAgent;
 
     @Mock
     private User mUser;
@@ -81,6 +79,7 @@ public class BuildDetailsImplTest {
 
     @Mock
     private Build mBuild;
+    private Agent mAgent = new Agent("name");
 
     private BuildDetails mBuildDetails;
 
@@ -234,7 +233,6 @@ public class BuildDetailsImplTest {
     @Test
     public void testGetAgentName() throws Exception {
         when(mBuild.getAgent()).thenReturn(mAgent);
-        when(mAgent.getName()).thenReturn("name");
         assertThat(mBuildDetails.getAgentName(), is(equalTo("name")));
     }
 
