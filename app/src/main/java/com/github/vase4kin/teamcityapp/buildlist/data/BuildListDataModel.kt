bc0d929e017231d95ac2159999385d92fb34d3a4
Andrey Tolpeev <andrey.tolpeev@gmail.com>
2019-08-14 22:49:27 +0800

    Convert build list java classes to kotlin

--- app/src/main/java/com/github/vase4kin/teamcityapp/buildlist/data/BuildListDataModel.kt
+++ app/src/main/java/com/github/vase4kin/teamcityapp/buildlist/data/BuildListDataModel.kt
@@ -1,11 +1,11 @@
 /*
- * Copyright 2016 Andrey Tolpeev
+ * Copyright 2019 Andrey Tolpeev
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
- *        http://www.apache.org/licenses/LICENSE-2.0
+ *    http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
@@ -14,17 +14,17 @@
  * limitations under the License.
  */
 
-package com.github.vase4kin.teamcityapp.buildlist.data;
+package com.github.vase4kin.teamcityapp.buildlist.data
 
-import com.github.vase4kin.teamcityapp.base.list.adapter.ModelLoadMore;
-import com.github.vase4kin.teamcityapp.base.list.view.BaseDataModel;
-import com.github.vase4kin.teamcityapp.buildlist.api.Build;
-import com.github.vase4kin.teamcityapp.overview.data.BuildDetails;
+import com.github.vase4kin.teamcityapp.base.list.adapter.ModelLoadMore
+import com.github.vase4kin.teamcityapp.base.list.view.BaseDataModel
+import com.github.vase4kin.teamcityapp.buildlist.api.Build
+import com.github.vase4kin.teamcityapp.overview.data.BuildDetails
 
 /**
  * Model managing build list data
  */
-public interface BuildListDataModel extends BaseDataModel, Iterable<BuildDetails>, ModelLoadMore<BuildListDataModel> {
+interface BuildListDataModel : BaseDataModel, Iterable<BuildDetails>, ModelLoadMore<BuildListDataModel> {
 
     /**
      * Get branch name
@@ -32,13 +32,13 @@ public interface BuildListDataModel extends BaseDataModel, Iterable<BuildDetails
      * @param position - Adapter position
      * @return
      */
-    String getBranchName(int position);
+    fun getBranchName(position: Int): String
 
     /**
      * @param position - adapter position
      * @return true if model has branch name
      */
-    boolean hasBranch(int position);
+    fun hasBranch(position: Int): Boolean
 
     /**
      * Get build status icon
@@ -46,7 +46,7 @@ public interface BuildListDataModel extends BaseDataModel, Iterable<BuildDetails
      * @param position - Adapter position
      * @return
      */
-    String getBuildStatusIcon(int position);
+    fun getBuildStatusIcon(position: Int): String
 
     /**
      * Get build status text
@@ -54,68 +54,68 @@ public interface BuildListDataModel extends BaseDataModel, Iterable<BuildDetails
      * @param position - Adapter position
      * @return
      */
-    String getStatusText(int position);
+    fun getStatusText(position: Int): String
 
     /**
      * @param position - Adapter position
      * @return
      */
-    String getBuildNumber(int position);
+    fun getBuildNumber(position: Int): String
 
     /**
      *
      * @param position - Adapter position
      * @return
      */
-    Build getBuild(int position);
+    fun getBuild(position: Int): Build
 
     /**
      * @param position - Adapter position
      * @return Build start date
      */
-    String getStartDate(int position);
+    fun getStartDate(position: Int): String
 
     /**
      * * @param position - Adapter position
      * @return Build type id
      */
-    String getBuildTypeId(int position);
+    fun getBuildTypeId(position: Int): String
 
     /**
      * @param position - Adapter position
      *
      * @return {true} if build has build type info
      */
-    boolean hasBuildTypeInfo(int position);
+    fun hasBuildTypeInfo(position: Int): Boolean
 
     /**
      * @param position - Adapter position
      *
      * @return return full name of configuration
      */
-    String getBuildTypeFullName(int position);
+    fun getBuildTypeFullName(position: Int): String
 
     /**
      * @param position - Adapter position
      * @return return name of configuration
      */
-    String getBuildTypeName(int position);
+    fun getBuildTypeName(position: Int): String
 
     /**
      * @param position - Adapter position
      * @return {true} if build is personal
      */
-    boolean isPersonal(int position);
+    fun isPersonal(position: Int): Boolean
 
     /**
      * @param position - Adapter position
      * @return {true} if build is pinned
      */
-    boolean isPinned(int position);
+    fun isPinned(position: Int): Boolean
 
     /**
      * @param position - Adapter position
      * @return {true} if build is queued
      */
-    boolean isQueued(int position);
+    fun isQueued(position: Int): Boolean
 }
---
+++ app/src/main/java/com/github/vase4kin/teamcityapp/buildlist/view/BuildListAdapter.kt
@@ -0,0 +1,48 @@
+/*
+ * Copyright 2019 Andrey Tolpeev
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.github.vase4kin.teamcityapp.buildlist.view
+
+import com.github.vase4kin.teamcityapp.base.list.adapter.BaseLoadMoreAdapter
+import com.github.vase4kin.teamcityapp.base.list.view.BaseViewHolder
+import com.github.vase4kin.teamcityapp.base.list.view.ViewHolderFactory
+import com.github.vase4kin.teamcityapp.buildlist.data.BuildListDataModel
+import com.github.vase4kin.teamcityapp.buildlist.data.OnBuildListPresenterListener
+
+/**
+ * Adapter for builds
+ */
+class BuildListAdapter(viewHolderFactories: Map<Int, ViewHolderFactory<BuildListDataModel>>) : BaseLoadMoreAdapter<BuildListDataModel>(viewHolderFactories) {
+
+    private var onBuildListPresenterListener: OnBuildListPresenterListener? = null
+
+    /**
+     * Set [OnBuildListPresenterListener]
+     *
+     * @param onBuildListPresenterListener - listener to set
+     */
+    fun setOnBuildListPresenterListener(onBuildListPresenterListener: OnBuildListPresenterListener) {
+        this.onBuildListPresenterListener = onBuildListPresenterListener
+    }
+
+    override fun onBindViewHolder(holder: BaseViewHolder<BuildListDataModel>, position: Int) {
+        super.onBindViewHolder(holder, position)
+        // Find the way how to make it through DI
+        if (holder is BuildViewHolder) {
+            holder.mContainer.setOnClickListener { onBuildListPresenterListener?.onBuildClick(dataModel.getBuild(position)) }
+        }
+    }
+}
