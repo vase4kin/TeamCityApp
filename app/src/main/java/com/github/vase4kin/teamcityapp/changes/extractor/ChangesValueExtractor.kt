47745bc109a1d281958c779fdff845aca43ee0e4
Andrey Tolpeev <andrey.tolpeev@gmail.com>
2019-08-15 21:38:40 +0800

    Convert changes java classes to kotlin

--- app/src/main/java/com/github/vase4kin/teamcityapp/changes/extractor/ChangesValueExtractor.kt
+++ app/src/main/java/com/github/vase4kin/teamcityapp/changes/extractor/ChangesValueExtractor.kt
@@ -1,11 +1,11 @@
 /*
- * Copyright 2016 Andrey Tolpeev
+ * Copyright 2019 Andrey Tolpeev
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
- *        http://www.apache.org/licenses/LICENSE-2.0
+ *    http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
@@ -14,19 +14,19 @@
  * limitations under the License.
  */
 
-package com.github.vase4kin.teamcityapp.changes.extractor;
+package com.github.vase4kin.teamcityapp.changes.extractor
 
-import com.github.vase4kin.teamcityapp.base.list.extractor.BaseValueExtractor;
+import com.github.vase4kin.teamcityapp.base.list.extractor.BaseValueExtractor
 
 /**
  * Change fragment value extractor
  */
-public interface ChangesValueExtractor extends BaseValueExtractor {
+interface ChangesValueExtractor : BaseValueExtractor {
 
     /**
      * Get changes url
      *
      * @return Url to load
      */
-    String getUrl();
+    val url: String
 }
