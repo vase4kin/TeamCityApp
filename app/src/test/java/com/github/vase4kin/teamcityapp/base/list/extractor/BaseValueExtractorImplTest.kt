8fa949cf4e29450a300ee481306faa818526bc92
Andrey Tolpeev <andrey.tolpeev@gmail.com>
2019-08-17 18:12:24 +0800

    Convert tests to kotlin

---
+++ app/src/test/java/com/github/vase4kin/teamcityapp/base/list/extractor/BaseValueExtractorImplTest.kt
@@ -0,0 +1,79 @@
+/*
+ * Copyright 2019 Andrey Tolpeev
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.github.vase4kin.teamcityapp.base.list.extractor
+
+import android.os.Bundle
+import com.github.vase4kin.teamcityapp.base.extractor.BundleExtractorValues
+import com.github.vase4kin.teamcityapp.buildlist.api.Build
+import com.github.vase4kin.teamcityapp.buildlist.filter.BuildListFilter
+import org.hamcrest.core.Is.`is`
+import org.junit.Assert.assertThat
+import org.junit.Before
+import org.junit.Test
+import org.mockito.Mock
+import org.mockito.Mockito.`when`
+import org.mockito.MockitoAnnotations
+
+class BaseValueExtractorImplTest {
+
+    @Mock
+    private lateinit var build: Build
+
+    @Mock
+    private lateinit var bundle: Bundle
+
+    @Mock
+    private lateinit var filter: BuildListFilter
+
+    private lateinit var valueExtractor: BaseValueExtractorImpl
+
+    @Before
+    fun setUp() {
+        MockitoAnnotations.initMocks(this)
+        valueExtractor = BaseValueExtractorImpl(bundle)
+    }
+
+    @Test
+    fun testGetId() {
+        `when`(bundle.getString(BundleExtractorValues.ID)).thenReturn("id")
+        assertThat(valueExtractor.id, `is`("id"))
+    }
+
+    @Test
+    fun testGetName() {
+        `when`(bundle.getString(BundleExtractorValues.NAME)).thenReturn("name")
+        assertThat(valueExtractor.name, `is`("name"))
+    }
+
+    @Test
+    fun testGetBuild() {
+        `when`(bundle.getSerializable(BundleExtractorValues.BUILD)).thenReturn(build)
+        assertThat(valueExtractor.buildDetails.toBuild(), `is`(build))
+    }
+
+    @Test
+    fun testGetBuildListFilter() {
+        `when`(bundle.getSerializable(BundleExtractorValues.BUILD_LIST_FILTER)).thenReturn(filter)
+        assertThat(valueExtractor.buildListFilter, `is`(filter))
+    }
+
+    @Test
+    fun testIsBundleNull() {
+        valueExtractor = BaseValueExtractorImpl(Bundle.EMPTY)
+        assertThat(valueExtractor.isBundleNullOrEmpty, `is`(true))
+    }
+}
---
+++ app/src/test/java/com/github/vase4kin/teamcityapp/base/tabs/presenter/BaseTabsPresenterImplTest.kt
@@ -0,0 +1,79 @@
+/*
+ * Copyright 2019 Andrey Tolpeev
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.github.vase4kin.teamcityapp.base.tabs.presenter
+
+import com.github.vase4kin.teamcityapp.base.tabs.data.BaseTabsDataManager
+import com.github.vase4kin.teamcityapp.base.tabs.view.BaseTabsViewModel
+import com.github.vase4kin.teamcityapp.base.tracker.ViewTracker
+import org.junit.Before
+import org.junit.Test
+import org.mockito.Matchers.eq
+import org.mockito.Mock
+import org.mockito.Mockito.verify
+import org.mockito.Mockito.verifyNoMoreInteractions
+import org.mockito.MockitoAnnotations
+
+class BaseTabsPresenterImplTest {
+
+    @Mock
+    private lateinit var viewModel: BaseTabsViewModel
+
+    @Mock
+    private lateinit var tracker: ViewTracker
+
+    @Mock
+    private lateinit var dataManager: BaseTabsDataManager
+
+    private lateinit var presenter: BaseTabsPresenterImpl<*, *, *>
+
+    @Before
+    fun setUp() {
+        MockitoAnnotations.initMocks(this)
+        presenter = BaseTabsPresenterImpl(viewModel, tracker, dataManager)
+    }
+
+    @Test
+    fun testHandleOnViewsCreated() {
+        presenter.onViewsCreated()
+        verify(viewModel).initViews()
+        verifyNoMoreInteractions(viewModel, tracker, dataManager)
+    }
+
+    @Test
+    fun testHandleOnViewsDestroyed() {
+        presenter.onViewsDestroyed()
+        verify(viewModel).unBindViews()
+        verifyNoMoreInteractions(viewModel, tracker, dataManager)
+    }
+
+    @Test
+    fun testHandleOnResume() {
+        presenter.onResume()
+        verify(dataManager).registerEventBus()
+        verify(dataManager).setListener(eq(presenter))
+        verify(tracker).trackView()
+        verifyNoMoreInteractions(viewModel, tracker, dataManager)
+    }
+
+    @Test
+    fun testHandleOnPause() {
+        presenter.onPause()
+        verify(dataManager).unregisterEventBus()
+        verify(dataManager).setListener(null)
+        verifyNoMoreInteractions(viewModel, tracker, dataManager)
+    }
+}
