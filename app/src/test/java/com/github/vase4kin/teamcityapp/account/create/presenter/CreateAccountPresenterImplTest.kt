b1835a266fbbac6318ee70ebc71647c46f04e3ae
Andrey Tolpeev <andrey.tolpeev@gmail.com>
2019-08-17 17:59:59 +0800

    Convert tests to kotlin

---
+++ app/src/test/java/com/github/vase4kin/teamcityapp/account/create/presenter/CreateAccountPresenterImplTest.kt
@@ -0,0 +1,230 @@
+/*
+ * Copyright 2019 Andrey Tolpeev
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.github.vase4kin.teamcityapp.account.create.presenter
+
+import com.github.vase4kin.teamcityapp.account.create.data.CreateAccountDataManager
+import com.github.vase4kin.teamcityapp.account.create.data.CreateAccountDataModel
+import com.github.vase4kin.teamcityapp.account.create.data.CustomOnLoadingListener
+import com.github.vase4kin.teamcityapp.account.create.data.OnLoadingListener
+import com.github.vase4kin.teamcityapp.account.create.router.CreateAccountRouter
+import com.github.vase4kin.teamcityapp.account.create.tracker.CreateAccountTracker
+import com.github.vase4kin.teamcityapp.account.create.view.CreateAccountView
+import com.github.vase4kin.teamcityapp.utils.capture
+import com.github.vase4kin.teamcityapp.utils.eq
+import org.junit.After
+import org.junit.Before
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.mockito.ArgumentCaptor
+import org.mockito.Captor
+import org.mockito.Mock
+import org.mockito.Mockito.*
+import org.mockito.runners.MockitoJUnitRunner
+
+@RunWith(MockitoJUnitRunner::class)
+class CreateAccountPresenterImplTest {
+
+    @Captor
+    lateinit var onLoadingListenerArgumentCaptor: ArgumentCaptor<OnLoadingListener<String>>
+
+    @Captor
+    lateinit var customOnLoadingListenerArgumentCaptor: ArgumentCaptor<CustomOnLoadingListener<String>>
+
+    @Mock
+    lateinit var view: CreateAccountView
+
+    @Mock
+    lateinit var dataManager: CreateAccountDataManager
+
+    @Mock
+    lateinit var dataModel: CreateAccountDataModel
+
+    @Mock
+    lateinit var router: CreateAccountRouter
+
+    @Mock
+    lateinit var tracker: CreateAccountTracker
+
+    private lateinit var presenter: CreateAccountPresenterImpl
+
+    @Before
+    fun setUp() {
+        presenter = CreateAccountPresenterImpl(view, dataManager, dataModel, router, tracker)
+    }
+
+    @After
+    fun tearDown() {
+        verifyNoMoreInteractions(view, dataManager, router, tracker)
+    }
+
+    @Test
+    fun testHandleOnCreateView() {
+        `when`(view.isEmailEmpty).thenReturn(true)
+        presenter.handleOnCreateView()
+        verify(view).initViews(presenter)
+    }
+
+    @Test
+    fun testOnClick() {
+        `when`(view.isEmailEmpty).thenReturn(false)
+        presenter.onClick()
+        verify(view).isEmailEmpty
+        verify(view).showDiscardDialog()
+    }
+
+    @Test
+    fun testHandleOnDestroyView() {
+        presenter.handleOnDestroy()
+        verify(view).onDestroyView()
+    }
+
+    @Test
+    fun testOnUserLoginButtonClickIfServerUrlIsNotProvided() {
+        presenter.validateUserData("", "userName", "password", false)
+        verify(view).hideError()
+        verify(view).showServerUrlCanNotBeEmptyError()
+    }
+
+    @Test
+    fun testOnUserLoginButtonClickIfUserNameIsNotProvided() {
+        presenter.validateUserData("url", "", "password", false)
+        verify(view).hideError()
+        verify(view).showUserNameCanNotBeEmptyError()
+    }
+
+    @Test
+    fun testOnUserLoginButtonClickIfPasswordIsNotProvided() {
+        presenter.validateUserData("url", "userName", "", false)
+        verify(view).hideError()
+        verify(view).showPasswordCanNotBeEmptyError()
+    }
+
+    @Test
+    fun testValidateUserUrlIfAccountExist() {
+        `when`(dataModel.hasAccountWithUrl("url", "userName")).thenReturn(true)
+        presenter.validateUserData("url", "userName", "password", false)
+        verify(view).hideError()
+        verify(view).showProgressDialog()
+        verify(dataModel).hasAccountWithUrl("url", "userName")
+        verify(view).showNewAccountExistErrorMessage()
+        verify(view).dismissProgressDialog()
+    }
+
+    @Test
+    fun testValidateGuestUserUrlIfAccountExist() {
+        `when`(dataModel.hasGuestAccountWithUrl("url")).thenReturn(true)
+        presenter.validateGuestUserData("url", false)
+        verify(view).hideError()
+        verify(view).showProgressDialog()
+        verify(dataModel).hasGuestAccountWithUrl("url")
+        verify(view).showNewAccountExistErrorMessage()
+        verify(view).dismissProgressDialog()
+    }
+
+    @Test
+    fun testValidateGuestUserUrlIfAccountIsNotExist() {
+        `when`(dataModel.hasGuestAccountWithUrl("url")).thenReturn(false)
+        presenter.validateGuestUserData("url", false)
+
+        verify(view).hideError()
+        verify(view).showProgressDialog()
+        verify(dataModel).hasGuestAccountWithUrl("url")
+        verify(dataManager).authGuestUser(capture(customOnLoadingListenerArgumentCaptor), eq("url"), eq(false), eq(false))
+
+        val listener = customOnLoadingListenerArgumentCaptor.value
+        listener.onSuccess("url")
+        verify(dataManager).saveGuestUserAccount("url", false)
+        verify(dataManager).initTeamCityService("url")
+        verify(view).dismissProgressDialog()
+        verify(view).finish()
+        verify(router).startRootProjectActivityWhenNewAccountIsCreated()
+        verify(tracker).trackGuestUserLoginSuccess(true)
+
+        listener.onFail(0, "error")
+        verify(view).showError("error")
+        verify(view, times(2)).dismissProgressDialog()
+        verify(tracker).trackGuestUserLoginFailed("error")
+    }
+
+    @Test
+    fun testValidateUserUrlIfAccountIsNotExist() {
+        `when`(dataModel.hasAccountWithUrl("url", "userName")).thenReturn(false)
+        presenter.validateUserData("url", "userName", "password", false)
+
+        verify(view).hideError()
+        verify(view).showProgressDialog()
+        verify(dataModel).hasAccountWithUrl("url", "userName")
+        verify(dataManager).authUser(customOnLoadingListenerArgumentCaptor.capture(), eq("url"), eq("userName"), eq("password"), eq(false), eq(false))
+
+        val customOnLoadingListener = customOnLoadingListenerArgumentCaptor.value
+        customOnLoadingListener.onSuccess("url")
+        verify(dataManager).saveNewUserAccount(eq("url"), eq("userName"), eq("password"), eq(false), onLoadingListenerArgumentCaptor.capture())
+
+        val loadingListener = onLoadingListenerArgumentCaptor.value
+        loadingListener.onSuccess("url")
+        verify(dataManager).initTeamCityService("url")
+        verify(view).dismissProgressDialog()
+        verify(view).finish()
+        verify(router).startRootProjectActivityWhenNewAccountIsCreated()
+        verify(tracker).trackUserLoginSuccess(true)
+
+        loadingListener.onFail("")
+        verify(view).showCouldNotSaveUserError()
+        verify(view, times(2)).dismissProgressDialog()
+        verify(tracker).trackUserDataSaveFailed()
+
+        customOnLoadingListener.onFail(0, "error")
+        verify(view).showError("error")
+        verify(view, times(3)).dismissProgressDialog()
+        verify(tracker).trackUserLoginFailed("error")
+    }
+
+    @Test
+    fun testOnGuestUserLoginButtonClickIfServerUrlIsNotProvided() {
+        presenter.validateGuestUserData("", false)
+        verify(view).hideError()
+        verify(view).showServerUrlCanNotBeEmptyError()
+    }
+
+    @Test
+    fun testDismissWithDiscardDialogIfEmailIsNotEmpty() {
+        `when`(view.isEmailEmpty).thenReturn(false)
+        presenter.finish()
+        verify(view).isEmailEmpty
+        verify(view).showDiscardDialog()
+    }
+
+    @Test
+    fun testDismissWithDiscardDialogIfEmailIsEmpty() {
+        `when`(view.isEmailEmpty).thenReturn(true)
+        presenter.finish()
+        verify(view).isEmailEmpty
+        verify(view).finish()
+    }
+
+    @Test
+    fun testHandleOnResume() {
+        presenter.handleOnResume()
+        verify(tracker).trackView()
+    }
+
+    @Test
+    fun testOnDisableSslSwitchClick() {
+        presenter.onDisableSslSwitchClick()
+        verify(view).showDisableSslWarningDialog()
+    }
+}
---
+++ app/src/test/java/com/github/vase4kin/teamcityapp/artifact/presenter/ArtifactPresenterImplTest.kt
@@ -0,0 +1,348 @@
+/*
+ * Copyright 2019 Andrey Tolpeev
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.github.vase4kin.teamcityapp.artifact.presenter
+
+import com.github.vase4kin.teamcityapp.account.create.data.OnLoadingListener
+import com.github.vase4kin.teamcityapp.artifact.api.File
+import com.github.vase4kin.teamcityapp.artifact.data.ArtifactDataManager
+import com.github.vase4kin.teamcityapp.artifact.extractor.ArtifactValueExtractor
+import com.github.vase4kin.teamcityapp.artifact.permissions.OnPermissionsResultListener
+import com.github.vase4kin.teamcityapp.artifact.permissions.PermissionManager
+import com.github.vase4kin.teamcityapp.artifact.router.ArtifactRouter
+import com.github.vase4kin.teamcityapp.artifact.view.ArtifactView
+import com.github.vase4kin.teamcityapp.artifact.view.OnPermissionsDialogListener
+import com.github.vase4kin.teamcityapp.base.tracker.ViewTracker
+import com.github.vase4kin.teamcityapp.overview.data.BuildDetails
+import com.github.vase4kin.teamcityapp.utils.any
+import com.github.vase4kin.teamcityapp.utils.capture
+import com.github.vase4kin.teamcityapp.utils.eq
+import org.hamcrest.MatcherAssert.assertThat
+import org.hamcrest.core.Is.`is`
+import org.junit.Before
+import org.junit.Test
+import org.mockito.ArgumentCaptor
+import org.mockito.Captor
+import org.mockito.Matchers.anyString
+import org.mockito.Mock
+import org.mockito.Mockito.*
+import org.mockito.MockitoAnnotations
+import java.util.*
+
+class ArtifactPresenterImplTest {
+
+    @Captor
+    private lateinit var onPermissionsDialogListenerArgumentCaptor: ArgumentCaptor<OnPermissionsDialogListener>
+
+    @Captor
+    private lateinit var argumentCaptor: ArgumentCaptor<OnLoadingListener<java.io.File>>
+
+    @Captor
+    private lateinit var onPermissionsResultListenerArgumentCaptor: ArgumentCaptor<OnPermissionsResultListener>
+
+    @Mock
+    private lateinit var children: File.Children
+
+    @Mock
+    private lateinit var content: File.Content
+
+    @Mock
+    private lateinit var ioFile: java.io.File
+
+    @Mock
+    private lateinit var file: File
+
+    @Mock
+    private lateinit var loadingListener: OnLoadingListener<List<File>>
+
+    @Mock
+    private lateinit var buildDetails: BuildDetails
+
+    @Mock
+    private lateinit var view: ArtifactView
+
+    @Mock
+    private lateinit var dataManager: ArtifactDataManager
+
+    @Mock
+    private lateinit var router: ArtifactRouter
+
+    @Mock
+    private lateinit var valueExtractor: ArtifactValueExtractor
+
+    @Mock
+    private lateinit var permissionManager: PermissionManager
+
+    @Mock
+    private lateinit var tracker: ViewTracker
+
+    private lateinit var presenter: ArtifactPresenterImpl
+
+    @Before
+    fun setUp() {
+        MockitoAnnotations.initMocks(this)
+        presenter = ArtifactPresenterImpl(view, dataManager, tracker, valueExtractor, router, permissionManager)
+    }
+
+    @Test
+    fun testCreateModel() {
+        val files = ArrayList<File>()
+        files.add(file)
+        val dataModel = presenter.createModel(files)
+        assertThat(dataModel.getFile(0), `is`(file))
+        assertThat(dataModel.itemCount, `is`(1))
+        verifyNoMoreInteractions(view, dataManager, router, valueExtractor, permissionManager, tracker)
+    }
+
+    @Test
+    fun testLoadData() {
+        `when`(valueExtractor.url).thenReturn("url")
+        presenter.loadData(loadingListener, false)
+        verify(valueExtractor).url
+        verify(dataManager).load(eq("url"), eq(loadingListener), eq(false))
+        verifyNoMoreInteractions(view, dataManager, router, valueExtractor, permissionManager, tracker)
+    }
+
+    @Test
+    fun testInitViews() {
+        presenter.initViews()
+        verify(view).setOnArtifactPresenterListener(eq(presenter))
+        verify(dataManager).registerEventBus()
+        verify(dataManager).setListener(eq(presenter))
+    }
+
+    @Test
+    fun testOnClickIfHasChildren() {
+        `when`(file.hasChildren()).thenReturn(true)
+        `when`(valueExtractor.buildDetails).thenReturn(buildDetails)
+        presenter.onClick(file)
+        verify(view).showFullBottomSheet(eq(file))
+        verifyNoMoreInteractions(view, dataManager, router, valueExtractor, permissionManager, tracker)
+    }
+
+    @Test
+    fun testOnClickIfHasChildrenAndIsFolder() {
+        `when`(file.hasChildren()).thenReturn(true)
+        `when`(file.isFolder).thenReturn(true)
+        `when`(file.children).thenReturn(children)
+        `when`(children.href).thenReturn("url")
+        `when`(valueExtractor.buildDetails).thenReturn(buildDetails)
+        presenter.onClick(file)
+        verify(valueExtractor).buildDetails
+        verify(router).openArtifactFile(eq(buildDetails), eq("url"))
+        verify(dataManager).unregisterEventBus()
+        verify(dataManager).setListener(null)
+        verifyNoMoreInteractions(view, dataManager, router, valueExtractor, permissionManager, tracker)
+    }
+
+    @Test
+    fun testOnClickIfBrowserUrl() {
+        `when`(file.hasChildren()).thenReturn(false)
+        `when`(file.href).thenReturn(".html")
+        `when`(valueExtractor.buildDetails).thenReturn(buildDetails)
+        presenter.onClick(file)
+        verify(view).showBrowserBottomSheet(eq(file))
+
+        `when`(file.href).thenReturn(".htm")
+        presenter.onClick(file)
+        verify(view, times(2)).showBrowserBottomSheet(eq(file))
+
+        verifyNoMoreInteractions(view, dataManager, router, valueExtractor, permissionManager, tracker)
+    }
+
+    @Test
+    fun testOnClickIfHasNoChildrenUrl() {
+        `when`(content.href).thenReturn("url")
+        `when`(file.hasChildren()).thenReturn(false)
+        `when`(file.href).thenReturn("url")
+        `when`(file.hasChildren()).thenReturn(false)
+        `when`(file.content).thenReturn(content)
+        `when`(file.name).thenReturn("name")
+        `when`(valueExtractor.buildDetails).thenReturn(buildDetails)
+        `when`(permissionManager.isWriteStoragePermissionsGranted).thenReturn(true)
+        `when`(dataManager.isTheFileApk(anyString())).thenReturn(false)
+
+        presenter.onClick(file)
+        verify(view).showDefaultBottomSheet(eq(file))
+
+        presenter.fileName = "name"
+        presenter.fileHref = "url"
+        presenter.downloadArtifactFile()
+        verify(view).showProgressDialog()
+        verify(permissionManager).isWriteStoragePermissionsGranted
+        verify(dataManager).isTheFileApk(eq("name"))
+        verify(dataManager).downloadArtifact(eq("url"), eq("name"), capture(argumentCaptor))
+
+        val onLoadingListener = argumentCaptor.value
+        onLoadingListener.onSuccess(ioFile)
+        verify(view).dismissProgressDialog()
+        verify(router).startFileActivity(eq(ioFile))
+
+        onLoadingListener.onFail("error")
+        verify(view, times(2)).dismissProgressDialog()
+        verify(dataManager).postArtifactErrorDownloadingEvent()
+
+        verifyNoMoreInteractions(view, dataManager, router, valueExtractor, permissionManager, tracker)
+    }
+
+    @Test
+    fun testOnLongClickIfFileHasChildrenAndNotAFolder() {
+        `when`(file.hasChildren()).thenReturn(true)
+        `when`(file.isFolder).thenReturn(false)
+        presenter.onLongClick(file)
+        verify(view).showFullBottomSheet(eq(file))
+        verifyNoMoreInteractions(view, dataManager, router, valueExtractor, tracker)
+    }
+
+    @Test
+    fun testOnLongClickIfFileIsAFolder() {
+        `when`(file.hasChildren()).thenReturn(false)
+        `when`(file.isFolder).thenReturn(true)
+        presenter.onLongClick(file)
+        verify(view).showFolderBottomSheet(eq(file))
+        verifyNoMoreInteractions(view, dataManager, router, valueExtractor, permissionManager, tracker)
+    }
+
+    @Test
+    fun testOnLongClickIfFileHasBrowserUrl() {
+        `when`(file.hasChildren()).thenReturn(false)
+        `when`(file.isFolder).thenReturn(false)
+        `when`(file.href).thenReturn(".htm")
+        presenter.onLongClick(file)
+        verify(view).showBrowserBottomSheet(eq(file))
+        verifyNoMoreInteractions(view, dataManager, router, valueExtractor, permissionManager, tracker)
+    }
+
+    @Test
+    fun testOnLongClick() {
+        `when`(file.hasChildren()).thenReturn(false)
+        `when`(file.isFolder).thenReturn(false)
+        `when`(file.href).thenReturn("url")
+        presenter.onLongClick(file)
+        verify(view).showDefaultBottomSheet(eq(file))
+        verifyNoMoreInteractions(view, dataManager, router, valueExtractor, permissionManager, tracker)
+    }
+
+    @Test
+    fun testOnViewsDestroyed() {
+        presenter.onViewsDestroyed()
+        verify(router).unbindCustomsTabs()
+    }
+
+    @Test
+    fun testOpenArtifactFile() {
+        `when`(valueExtractor.buildDetails).thenReturn(buildDetails)
+        presenter.onOpenArtifactEvent("href")
+        verify(valueExtractor).buildDetails
+        verify(router).openArtifactFile(eq(buildDetails), eq("href"))
+        verify(dataManager).unregisterEventBus()
+        verify(dataManager).setListener(null)
+        verifyNoMoreInteractions(view, dataManager, router, valueExtractor, permissionManager, tracker)
+    }
+
+    @Test
+    fun testStartBrowser() {
+        `when`(valueExtractor.buildDetails).thenReturn(buildDetails)
+        presenter.onStartBrowserEvent("url")
+        verify(valueExtractor).buildDetails
+        verify(router).startBrowser(eq(buildDetails), eq("url"))
+        verifyNoMoreInteractions(view, dataManager, router, valueExtractor, permissionManager, tracker)
+    }
+
+    @Test
+    fun testUnSubscribe() {
+        presenter.unSubscribe()
+        verify(dataManager).unsubscribe()
+        verifyNoMoreInteractions(view, dataManager, router, valueExtractor, permissionManager, tracker)
+    }
+
+    @Test
+    fun testHandleOnRequestPermissionsResult() {
+        `when`(permissionManager.isWriteStoragePermissionsGranted).thenReturn(false)
+        val requestCode = 12
+        val permissions = arrayOf("123")
+        val grantResults = intArrayOf(12, 123)
+        presenter.fileName = ""
+        presenter.fileHref = ""
+        presenter.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        verify(permissionManager).onRequestPermissionsResult(eq(requestCode), eq(permissions), eq(grantResults), capture(onPermissionsResultListenerArgumentCaptor))
+        val listener = onPermissionsResultListenerArgumentCaptor.value
+        listener.onDenied()
+        verify(view).showPermissionsDeniedDialog()
+        listener.onGranted()
+        verify(permissionManager).isWriteStoragePermissionsGranted
+        verify(view).showPermissionsInfoDialog(capture(onPermissionsDialogListenerArgumentCaptor))
+        val onPermissionsDialogListener = onPermissionsDialogListenerArgumentCaptor.value
+        onPermissionsDialogListener.onAllow()
+        verify(permissionManager).requestWriteStoragePermissions()
+        verifyNoMoreInteractions(view, dataManager, router, valueExtractor, permissionManager, tracker)
+    }
+
+    @Test
+    fun testDownloadArtifactFileIfUserDoesNotHaveRequiredPermissionsAndNeedToShowTheDialogInfo() {
+        `when`(permissionManager.isWriteStoragePermissionsGranted).thenReturn(false)
+        presenter.onDownloadArtifactEvent("", "")
+        verify(permissionManager).isWriteStoragePermissionsGranted
+        verify(view).showPermissionsInfoDialog(capture(onPermissionsDialogListenerArgumentCaptor))
+        val listener = onPermissionsDialogListenerArgumentCaptor.value
+        listener.onAllow()
+        verify(permissionManager).requestWriteStoragePermissions()
+        verifyNoMoreInteractions(view, dataManager, router, valueExtractor, permissionManager, tracker)
+    }
+
+    @Test
+    fun testDownloadArtifactFileIfUserDoesNotHaveRequiredPermissionsAndNoNeedToShowTheDialogInfo() {
+        `when`(permissionManager.isWriteStoragePermissionsGranted).thenReturn(false)
+        presenter.onDownloadArtifactEvent("", "")
+        verify(permissionManager).isWriteStoragePermissionsGranted
+        verify(view).showPermissionsInfoDialog(capture(onPermissionsDialogListenerArgumentCaptor))
+        val listener = onPermissionsDialogListenerArgumentCaptor.value
+        listener.onAllow()
+        verify(permissionManager).requestWriteStoragePermissions()
+        verifyNoMoreInteractions(view, dataManager, router, valueExtractor, permissionManager, tracker)
+    }
+
+    @Test
+    fun testDownloadArtifactFileIfThFileIsApk() {
+        `when`(permissionManager.isWriteStoragePermissionsGranted).thenReturn(true)
+        `when`(permissionManager.isInstallPackagesPermissionGranted).thenReturn(false)
+        `when`(dataManager.isTheFileApk(anyString())).thenReturn(true)
+        presenter.onDownloadArtifactEvent("", "")
+        verify(permissionManager).isWriteStoragePermissionsGranted
+        verify(dataManager).isTheFileApk(eq(""))
+        verify(permissionManager).isInstallPackagesPermissionGranted
+        verify(view).showInstallPackagesPermissionsInfoDialog(capture(onPermissionsDialogListenerArgumentCaptor))
+        val listener = onPermissionsDialogListenerArgumentCaptor.value
+        listener.onAllow()
+        verify(permissionManager).requestInstallPackagesPermission()
+        verifyNoMoreInteractions(view, dataManager, router, valueExtractor, permissionManager, tracker)
+    }
+
+    @Test
+    fun testDownloadArtifactFileIfThFileIsApk2() {
+        `when`(permissionManager.isWriteStoragePermissionsGranted).thenReturn(true)
+        `when`(permissionManager.isInstallPackagesPermissionGranted).thenReturn(true)
+        `when`(dataManager.isTheFileApk(eq("name.apk"))).thenReturn(true)
+        presenter.onDownloadArtifactEvent("name.apk", "url")
+        verify(permissionManager).isWriteStoragePermissionsGranted
+        verify(dataManager).isTheFileApk(eq("name.apk"))
+        verify(permissionManager).isInstallPackagesPermissionGranted
+        verify(view).showProgressDialog()
+        verify(dataManager).downloadArtifact(eq("url"), eq("name.apk"), any())
+        verifyNoMoreInteractions(view, dataManager, router, valueExtractor, permissionManager, tracker)
+    }
+
+}
