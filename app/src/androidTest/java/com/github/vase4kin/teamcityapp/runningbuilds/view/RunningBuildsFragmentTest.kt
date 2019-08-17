e9cb9bc412a04e6354bb2fb37e4814f36abb27a9
Andrey Tolpeev <andrey.tolpeev@gmail.com>
2019-08-17 20:21:38 +0800

    Fix UI tests compilation

--- app/src/androidTest/java/com/github/vase4kin/teamcityapp/runningbuilds/view/RunningBuildsFragmentTest.kt
+++ app/src/androidTest/java/com/github/vase4kin/teamcityapp/runningbuilds/view/RunningBuildsFragmentTest.kt
@@ -1,11 +1,11 @@
 /*
- * Copyright 2016 Andrey Tolpeev
+ * Copyright 2019 Andrey Tolpeev
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
- *        http://www.apache.org/licenses/LICENSE-2.0
+ *    http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
@@ -67,7 +67,7 @@ class RunningBuildsFragmentTest {
             .addComponentDependency(AppComponent::class.java, AppModule(InstrumentationRegistry.getInstrumentation().targetContext.applicationContext as TeamCityApplication))
             .set { restApiComponent ->
                 val app = InstrumentationRegistry.getInstrumentation().targetContext.applicationContext as TeamCityApplication
-                app.restApiInjector = restApiComponent
+                app.setRestApiInjector(restApiComponent)
             }
 
     @Rule
