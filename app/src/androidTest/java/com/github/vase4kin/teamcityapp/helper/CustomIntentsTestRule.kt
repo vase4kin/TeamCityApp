255abfb35819a3b98cee959308f6264758d9fef9
Andrey Tolpeev <andrey.tolpeev@gmail.com>
2019-09-06 00:25:32 +0800

    Fix compilation

--- app/src/androidTest/java/com/github/vase4kin/teamcityapp/helper/CustomIntentsTestRule.kt
+++ app/src/androidTest/java/com/github/vase4kin/teamcityapp/helper/CustomIntentsTestRule.kt
@@ -18,28 +18,10 @@ package com.github.vase4kin.teamcityapp.helper
 
 import android.app.Activity
 
-import androidx.test.espresso.Espresso
-import androidx.test.espresso.base.DefaultFailureHandler
 import androidx.test.espresso.intent.rule.IntentsTestRule
-import androidx.test.platform.app.InstrumentationRegistry
-
-import org.junit.runner.Description
-import org.junit.runners.model.Statement
 
 /**
  * Simple IntentsTestRule with spoon screenshot support on failure
  */
 class CustomIntentsTestRule<T : Activity>(activityClass: Class<T>) :
-    IntentsTestRule<T>(activityClass, false, false) {
-
-    override fun apply(base: Statement, description: Description): Statement {
-        val testClassName = description.className
-        val testMethodName = description.methodName
-        val context = InstrumentationRegistry.getInstrumentation().targetContext
-        Espresso.setFailureHandler { throwable, matcher ->
-            FalconScreenshotAction.perform("failure", testClassName, testMethodName)
-            DefaultFailureHandler(context).handle(throwable, matcher)
-        }
-        return super.apply(base, description)
-    }
-}
+    IntentsTestRule<T>(activityClass, false, false)
