ec6c2f05a405036017f000da1e70419b4b7c1b28
Andrey Tolpeev <andrey.tolpeev@gmail.com>
2019-09-06 22:41:38 +0800

    Introduce new bottom tab to show agents

--- app/src/main/java/com/github/vase4kin/teamcityapp/app_navigation/AppNavigationItem.kt
+++ app/src/main/java/com/github/vase4kin/teamcityapp/app_navigation/AppNavigationItem.kt
@@ -24,5 +24,6 @@ enum class AppNavigationItem(@DrawableRes val icon: Int, @StringRes val title: I
     PROJECTS(R.drawable.ic_home_white_24dp, R.string.projects_drawer_item),
     FAVORITES(R.drawable.ic_favorite_white_24dp, R.string.favorites_drawer_item),
     RUNNING_BUILDS(R.drawable.ic_directions_run_white_24px, R.string.running_builds_drawer_item),
-    BUILD_QUEUE(R.drawable.ic_layers_white_24dp, R.string.build_queue_drawer_item)
+    BUILD_QUEUE(R.drawable.ic_layers_white_24dp, R.string.build_queue_drawer_item),
+    AGENTS(R.drawable.ic_directions_railway_black_24dp, R.string.agents_drawer_item)
 }
--- app/src/main/java/com/github/vase4kin/teamcityapp/app_navigation/FragmentFactory.kt
+++ app/src/main/java/com/github/vase4kin/teamcityapp/app_navigation/FragmentFactory.kt
@@ -17,13 +17,12 @@
 package com.github.vase4kin.teamcityapp.app_navigation
 
 import androidx.fragment.app.Fragment
+import com.github.vase4kin.teamcityapp.agents.view.AgentListFragment
 import com.github.vase4kin.teamcityapp.favorites.view.FavoritesFragment
 import com.github.vase4kin.teamcityapp.home.router.HomeRouter
 import com.github.vase4kin.teamcityapp.navigation.view.NavigationListFragment
 import com.github.vase4kin.teamcityapp.queue.view.BuildQueueFragment
 import com.github.vase4kin.teamcityapp.runningbuilds.view.RunningBuildsFragment
-
-private const val FRAGMENTS_SIZE = 4
 
 interface FragmentFactory {
     fun createFragment(index: Int): Fragment
@@ -41,9 +40,10 @@ class FragmentFactoryImpl : FragmentFactory {
             AppNavigationItem.FAVORITES.ordinal -> return FavoritesFragment()
             AppNavigationItem.RUNNING_BUILDS.ordinal -> return RunningBuildsFragment()
             AppNavigationItem.BUILD_QUEUE.ordinal -> return BuildQueueFragment()
+            AppNavigationItem.AGENTS.ordinal -> return AgentListFragment.newInstance(false)
         }
         throw IllegalStateException("Wrong index")
     }
 
-    override fun getSize(): Int = FRAGMENTS_SIZE
+    override fun getSize(): Int = AppNavigationItem.values().size
 }
