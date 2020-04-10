4391bd9d65523fd837e3dce9801ae6649074e851
Andrey Tolpeev <andrey.tolpeev@gmail.com>
2020-04-10 12:19:23 +0800

    Prepare release 1.5.1.1

--- buildSrc/src/main/kotlin/teamcityapp/buildsrc/Dependencies.kt
+++ buildSrc/src/main/kotlin/teamcityapp/buildsrc/Dependencies.kt
@@ -23,8 +23,8 @@ object Config {
     const val compileSdk = 29
     const val targetSdk = 29
     const val buildTools = "29.0.2"
-    const val versionCode = 106
-    const val versionName = "1.5.1.0"
+    const val versionCode = 107
+    const val versionName = "1.5.1.1"
     const val applicationId = "com.github.vase4kin.teamcityapp"
     val javaVersion = JavaVersion.VERSION_1_8
 }
